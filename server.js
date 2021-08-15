f225682c0233febcb2f130e936a9979b4878680d
Deepinder Hehar <60627733+DeepinderH@users.noreply.github.com>
2021-08-14 22:58:21 -0600

    updating database

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const knex = require('knex')
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-colorful-43276',
     user : 'postgres',
     password : 'password',
     database : 'smart-brain'
